010cc66107ba6963b5d1f9340ff8e341916a15d5
rosannesanfilippo-dot <rosanne.sanfilippo@gmail.com>
2026-01-20 11:21:54 -0800

    Create bookDates.js

---
+++ netlify/bookDates.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+exports.handler = async (event) => {
+  const { date, name } = JSON.parse(event.body);
+
+  const baseId = process.env.AIRTABLE_BASE_ID;
+
+  const url = `https://api.airtable.com/v0/${baseId}/Dates`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      fields: { Date: date, Name: name }
+    })
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+};
